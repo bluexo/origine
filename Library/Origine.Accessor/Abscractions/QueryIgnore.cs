eb9cb6849f55c7dd56bed0e37d9614ab4d726b55
Alvin <bluexo@hotmail.com>
2020-04-16 15:20:45 +0800

    Add all solutions to src folder

--- Library/Origine.Accessor/Abscractions/QueryIgnore.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Origine
-{
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
-    public class QueryIgnore : Attribute { }
-}
